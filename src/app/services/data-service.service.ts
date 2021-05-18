d6cc2455cd94312972fd996b9015278ea72b2b05
Rashinka Fernando <rashinkaprameshi@gmail.com>
2021-05-18 22:13:47 +0530

    added data url to service

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataServiceService {
 
-  constructor() { }
+  private gloabelDataUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/12-31-2020.csv';
+  constructor(private http : HttpClient) { }
+
+  getGlobalData(){
+    this.http.get(this.gloabelDataUrl).pipe(
+      map
+    )
+  }
 }
