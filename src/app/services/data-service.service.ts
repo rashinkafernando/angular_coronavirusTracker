c1572639706ca5c87da87772dc488778167ea568
Rashinka Fernando <rashinkaprameshi@gmail.com>
2021-05-18 22:43:58 +0530

    splitting rows in the data

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -13,7 +13,16 @@ export class DataServiceService {
   getGlobalData(){
     return this.http.get(this.gloabelDataUrl, {responseType : 'text'}).pipe(
       map(result => {
-        
+
+        let rows = result.split('\n');
+        //console.log(rows);
+
+        rows.forEach(row => {
+          let cols = row.split(/,(?=\S)/)
+          console.log(cols);
+        }
+        )
+        return [];
       }) 
     )
   }
