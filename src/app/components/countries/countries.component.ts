7972f81aff33f148ce98b9d85475a4016754a859
Rashinka Fernando <rashinkaprameshi@gmail.com>
2021-05-19 22:32:29 +0530

    fetched country names to drop down

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalDataSummary } from 'src/app/models/globalData';
 import { DataServiceService } from 'src/app/services/data-service.service';
 
 @Component({
@@ -8,9 +9,18 @@ import { DataServiceService } from 'src/app/services/data-service.service';
 })
 export class CountriesComponent implements OnInit {
 
-  constructor() { }
+  data : GlobalDataSummary[] ;
+  countries : string[] = []; //empty string array
+  constructor(private service : DataServiceService) { }
 
   ngOnInit(): void {
+
+    this.service.getGlobalData().subscribe(result => {
+      this.data = result;
+      this.data.forEach(cs => {
+        this.countries.push(cs.country);
+      })
+    })
   }
 
 }
